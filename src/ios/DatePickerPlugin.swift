7a98f8307b8d199b84ff3f9fe0649118d9b41e48
saiprasanthr <er.saiprasanthr@gmail.com>
2021-06-13 14:48:05 +0530

    bug fixed

--- src/ios/DatePickerPlugin.swift
+++ src/ios/DatePickerPlugin.swift
@@ -2,31 +2,31 @@ import UIKit
 
 @objc(DatePickerPlugin) 
 class DatePickerPlugin : CDVPlugin{
-
-var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-
-@objc(createDatePicker:) 
-func createDatePicker(command: CDVInvokedUrlCommand) {
+    
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    
+    @objc(createDatePicker:) 
+    func createDatePicker(command: CDVInvokedUrlCommand) {
         // Create a DatePicker
         let datePicker: UIDatePicker = UIDatePicker()
         
         // Posiiton date picket within a view
-        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 100)
+        datePicker.frame = CGRect(x: 10, y: 50, width: self.viewController.view.frame.width, height: 100)
         
         // Set some of UIDatePicker properties
         datePicker.timeZone = NSTimeZone.local
         datePicker.backgroundColor = UIColor.white
         
         // Add an event to call onDidChangeDate function when value is changed.
-        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
+        datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(_:)), for: .valueChanged)
         
         // Add DataPicker to the view
-        self.view.addSubview(datePicker)
+        self.viewController.view.addSubview(datePicker)
         
         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "DatePicker Created")
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
     }
-
+    
     @objc func datePickerValueChanged(_ sender: UIDatePicker){
         
         // Create date formatter
