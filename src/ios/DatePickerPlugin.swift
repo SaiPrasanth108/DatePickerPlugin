1227da2d736af02475f3779600841385cfd77861
saiprasanthr <er.saiprasanthr@gmail.com>
2021-06-13 14:23:06 +0530

    ios datepicker plugin completed

---
+++ src/ios/DatePickerPlugin.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+@objc(DatePickerPlugin) 
+class DatePickerPlugin : CDVPlugin{
+
+var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+
+@objc(createDatePicker:) 
+func createDatePicker(command: CDVInvokedUrlCommand) {
+        // Create a DatePicker
+        let datePicker: UIDatePicker = UIDatePicker()
+        
+        // Posiiton date picket within a view
+        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 100)
+        
+        // Set some of UIDatePicker properties
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.backgroundColor = UIColor.white
+        
+        // Add an event to call onDidChangeDate function when value is changed.
+        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
+        
+        // Add DataPicker to the view
+        self.view.addSubview(datePicker)
+        
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "DatePicker Created")
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+        
+        // Create date formatter
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        // Set date format
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        
+        // Apply date format
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        print("Selected value \(selectedDate)")
+    }
+}
--- www/DatePickerPlugin.js
+++ www/DatePickerPlugin.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'DatePickerPlugin', 'coolMethod', [arg0]);
+  exec(success, error, 'DatePickerPlugin', 'coolMethod', [arg0]);
+};
+
+exports.createDatePicker = function (arg0, success, error) {
+  exec(success, error, 'DatePickerPlugin', 'createDatePicker', [arg0]);
 };
