c1eb93fb3a5f091e631891f8d4797acc1ac53203
saiprasanthr <er.saiprasanthr@gmail.com>
2021-06-13 19:27:55 +0530

    dynamic constraints and label

--- src/ios/DatePickerPlugin.swift
+++ src/ios/DatePickerPlugin.swift
@@ -7,21 +7,48 @@ class DatePickerPlugin : CDVPlugin{
     
     @objc(createDatePicker:) 
     func createDatePicker(command: CDVInvokedUrlCommand) {
-        // Create a DatePicker
+        
+        let label: UILabel = UILabel()
+        label.text = command.arguments[0] as? String ?? ""
+      
+        
         let datePicker: UIDatePicker = UIDatePicker()
-        
-        // Posiiton date picket within a view
-        datePicker.frame = CGRect(x: 10, y: 50, width: self.viewController.view.frame.width, height: 100)
-        
-        // Set some of UIDatePicker properties
         datePicker.timeZone = NSTimeZone.local
         datePicker.backgroundColor = UIColor.white
-        
-        // Add an event to call onDidChangeDate function when value is changed.
         datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(_:)), for: .valueChanged)
         
-        // Add DataPicker to the view
-        self.viewController.view.addSubview(datePicker)
+        
+        let stackView   = UIStackView()
+        stackView.axis  = NSLayoutConstraint.Axis.horizontal
+        stackView.distribution  = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing   = 10.0
+        
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(datePicker)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.viewController.view.addSubview(stackView)
+        
+        //Constraints
+        let top            = command.arguments[1] as? CGFloat ?? 0
+        let topIsActive    = command.arguments[2] as? Bool ?? false
+        let bottom         = command.arguments[3] as? CGFloat ?? 0
+        let bottomIsActive = command.arguments[4] as? Bool ?? false
+        let left           = command.arguments[5] as? CGFloat ?? 0
+        let leftIsActive   = command.arguments[6] as? Bool ?? false
+        let right          = command.arguments[7] as? CGFloat ?? 0
+        let rightIsActive  = command.arguments[8] as? Bool ?? false
+        let centerX        = command.arguments[9] as? Bool ?? false
+        let centerY        = command.arguments[10] as? Bool ?? false
+        
+        guard let myView = self.viewController.view else { return }
+        stackView.topAnchor.constraint(equalTo: myView.topAnchor, constant: top).isActive = topIsActive
+        stackView.bottomAnchor.constraint(equalTo: myView.bottomAnchor, constant: bottom).isActive = bottomIsActive
+        stackView.leftAnchor.constraint(equalTo: myView.leftAnchor, constant: left).isActive = leftIsActive
+        stackView.rightAnchor.constraint(equalTo: myView.rightAnchor, constant: right).isActive = rightIsActive
+        stackView.centerXAnchor.constraint(equalTo: myView.centerXAnchor).isActive = centerX
+        stackView.centerYAnchor.constraint(equalTo: myView.centerYAnchor).isActive = centerY
         
         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "DatePicker Created")
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
--- www/DatePickerPlugin.js
+++ www/DatePickerPlugin.js
@@ -4,6 +4,32 @@ exports.coolMethod = function (arg0, success, error) {
   exec(success, error, 'DatePickerPlugin', 'coolMethod', [arg0]);
 };
 
-exports.createDatePicker = function (arg0, success, error) {
-  exec(success, error, 'DatePickerPlugin', 'createDatePicker', [arg0]);
+exports.createDatePicker = function (
+  labelName,
+  topConstraint,
+  topActive,
+  bottomConstraint,
+  bottomActive,
+  leftConstraint,
+  leftActive,
+  rightConstraint,
+  rightActive,
+  centerX,
+  centerY,
+  success,
+  error
+) {
+  exec(success, error, 'DatePickerPlugin', 'createDatePicker', [
+    labelName,
+    topConstraint,
+    topActive,
+    bottomConstraint,
+    bottomActive,
+    leftConstraint,
+    leftActive,
+    rightConstraint,
+    rightActive,
+    centerX,
+    centerY,
+  ]);
 };
